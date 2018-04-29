99aa52eaebb99aa501b720c46852dd2404977d02
Muharrem Kaçkın <muharremkackin@gmail.com>
2018-04-29 13:03:45 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, position)
-  if(board[position] == " ")
+  if board[position] == " "
+    return false
+  elseif board[position] == ""
     return false
   else
     return true
